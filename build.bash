62ac0cef974bce241fc4fd08e4d8e38bc1b80f14
claus <cmrosito@fi.uba.ar>
2024-08-10 15:20:08 -0300

    chore: Update build.bash for ROS 2 development environment setup

--- build.bash
+++ build.bash
@@ -3,4 +3,8 @@ source setup.env
 
 echo "Building image $IMAGE_NAME using USERNAME=$USERNAME USER_UID=$USER_UID USER_GID=$USER_GID"
 
-docker build --build-arg="USER_UID=$USER_UID" --build-arg="USER_GID=$USER_GID" --build-arg="USERNAME=$USERNAME" -t $IMAGE_NAME:latest . 
+docker build --build-arg="USER_UID=$USER_UID" \
+    --build-arg="USER_GID=$USER_GID" \
+    --build-arg="USERNAME=$USERNAME" \
+    --build-arg=¨ROS_DISTRO=$ROS_DISTRO" \
+    -t $IMAGE_NAME:latest .
